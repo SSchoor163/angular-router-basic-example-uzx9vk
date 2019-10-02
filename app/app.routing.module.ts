d2462cdbc508460b81b3f6fce0723ea33eee7956
SSchoor163 <steven_schoor@yahoo.com>
2019-10-02 14:28:39 -0500

    Initial commit

---
+++ app/app.routing.module.ts
@@ -0,0 +1,28 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LoginViewComponent } from './views/login/login.component';
+import { HomeViewComponent } from './views/home/home.component';
+import { CatalogViewComponent } from './views/catalog/catalog.component';
+
+@NgModule({
+  declarations: [ 
+    LoginViewComponent, HomeViewComponent, CatalogViewComponent
+  ],
+  imports: [
+    RouterModule.forRoot([
+      { path: 'login', component: LoginViewComponent },
+      { path: 'home', component: HomeViewComponent },
+      { path: 'catalog', component: CatalogViewComponent },
+      { path: '**', redirectTo: 'login' },
+      {path: 'users', component: UsersViewComponent}
+    ])
+  ],
+  exports: [
+    RouterModule,
+  ],
+  providers: [],
+
+})
+export class AppRoutingModule {}
+
+
